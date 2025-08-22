de24fa27126ebbefef53016ef792e3de7c523bfa
Boy van Ooijen <boyvanooijen@Boys-MBP.fritz.box>
2025-08-22 07:25:59 +0200

    edit allowed hosts

--- wms_api/wms_api/settings/production.py
+++ wms_api/wms_api/settings/production.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY", default="django-insecure-change-me")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=list)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default='localhost').split(',')
 
 API_BASE_URL = config("API_BASE_URL", default="http://localhost:8000/api/")
 
